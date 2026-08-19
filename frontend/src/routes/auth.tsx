5572083f719395e34157a50ac73a83f3f9869aa0
Louis Bui <louis@positive.fi>
2026-08-19 17:56:34 +0300

    Make auth route accept cross-domain handoff

--- frontend/src/routes/auth.tsx
+++ frontend/src/routes/auth.tsx
@@ -2,6 +2,15 @@ import { createFileRoute, Outlet } from "@tanstack/react-router";
 import { z } from "zod";
 
 export const Route = createFileRoute("/auth")({
-  validateSearch: z.object({ idle: z.enum(["1"]).optional() }).parse,
+  validateSearch: z
+    .object({
+      idle: z.enum(["1"]).optional(),
+      sso: z.enum(["miss"]).optional(),
+      token_hash: z.string().optional(),
+      type: z.enum(["email", "magiclink"]).optional(),
+      returnTo: z.enum(["/auth", "/auth/login"]).optional(),
+    })
+    .passthrough()
+    .parse,
   component: () => <Outlet />,
 });
