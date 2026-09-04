8db6eb892e92184a499c3d2f9b338e683b01efcf
Louis Bui <louis@positive.fi>
2026-09-04 07:49:29 +0300

    Route legacy STG-LM import through universal transport

---
+++ frontend/src/lib/stg-lm.client/index.ts
@@ -0,0 +1,6 @@
+export { requestStgClose, requestStgFollowup } from "@/lib/stg-lm";
+export type {
+  ReflectionCloseReply as CloseReply,
+  ReflectionFollowupReply as FollowupReply,
+  StgLmLanguage as StgLanguage,
+} from "@/lib/stg-lm";
